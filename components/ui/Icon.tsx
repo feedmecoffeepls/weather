d7fa547c63aa4fc3198eef7073f9cfaea28b0c48
Isaac <isaac.quek@gmail.com>
2024-05-14 21:51:30 +0800

    Added popover text for mobile only

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 interface IconProps {
   iconId: string;
@@ -11,7 +12,21 @@ const Icon = ({ iconId, alt, iconSize = 64 }: IconProps) => {
   const size = ".png";
   const constructed_url = `${base_url}${iconId}${size}`;
   return (
-    <Image src={constructed_url} alt={alt} width={iconSize} height={iconSize} />
+    <div className="pointer-events-auto sm:pointer-events-none">
+      <Popover>
+        <PopoverTrigger>
+          <Image
+            src={constructed_url}
+            alt={alt}
+            width={iconSize}
+            height={iconSize}
+          />
+        </PopoverTrigger>
+        <PopoverContent>
+          <p className="text-center font-bold capitalize">{alt}</p>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 };
 
